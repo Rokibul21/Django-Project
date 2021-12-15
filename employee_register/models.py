5171acba87423a990a5a7e121106e103c0478337
Rokibul21 <rokibul.rabby890@gmail.com>
2021-12-15 15:25:27 +0600

    Some Chnageg in model

--- employee_register/models.py
+++ employee_register/models.py
@@ -3,9 +3,9 @@ from django.http import request
 
 
 class StudentData(models.Model):
-    id=models.AutoField(primary_key=True)
-    name=models.CharField(max_length=255)
-    email=models.CharField(max_length=255) 
+    id             = models.AutoField(primary_key=True)
+    name           = models.CharField(max_length=255)
+    email          = models.CharField(max_length=255) 
     genders = (
         ('male', 'male'),
         ('female', 'female'),
@@ -13,8 +13,8 @@ class StudentData(models.Model):
     )
 
     gender          = models.CharField(max_length=50, choices=genders)
-    created_at=models.DateTimeField(auto_now_add=True)
-    objects=models.Manager()
+    created_at      = models.DateTimeField(auto_now_add=True)
+    objects         = models.Manager()
 
   
     
